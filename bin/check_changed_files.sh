31e235bafcb0ccb2346afb079484fca772680394
Alessandro Franceschi <al@example42.com>
2020-04-14 20:27:59 +0200

    Fix ci

--- bin/check_changed_files.sh
+++ bin/check_changed_files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-dirs=$*
+dirs=$(echo $* | sed 's/ /|/g')
 exit_code=0
 changed_files=$(git  diff --name-only ${CI_COMMIT_BEFORE_SHA}...${CI_COMMIT_SHA})
 failed_files=""
@@ -7,13 +7,11 @@ failed_files=""
 echo "Safe paths: ${dirs}"
 echo -e "Changed files in MR:\n$changed_files"
 for file in $changed_files; do
-  for dir in $dirs; do
-    echo "$file" | grep "^${dir}" 
-    if [ "$?" != "0" ]; then
-      exit_code=1
-      failed_files+="${file}\n"
-    fi
-  done
+  echo "$file" | grep -E -w "${dirs}" 
+  if [ "$?" != "0" ]; then
+    exit_code=1
+    failed_files+="${file}\n"
+  fi
 done
 
 echo -e "Changed files not in safe paths:\n$failed_files"
